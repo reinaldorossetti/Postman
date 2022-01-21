c91e38ce6cdddedadab2984994e269226d571859
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2022-01-21 20:06:41 -0300

    adicionando package json e script de testes em paralelo

--- newman_tests.js
+++ newman_tests.js
@@ -4,8 +4,8 @@ global.Promise = require('bluebird');
 const path = require('path');
 const newman =  Promise.promisifyAll(require('newman'));
 const fs = Promise.promisifyAll(require('fs'));
-const environment = 'postman_environment.json';
-const FOLDER = path.join(__dirname, 'Collections_Folder');
+//const environment = 'postman_environment.json';
+const FOLDER = path.join(__dirname, 'collections'); 
 
 
 let files = fs.readdirSync(FOLDER);
@@ -15,7 +15,7 @@ console.log(files);
 Promise.map(files, file => {
     return newman.runAsync({
     collection: file, // your collection
-    environment: path.join(__dirname, environment), //your env
+    //environment: path.join(__dirname, environment), //your env
     reporters: ['cli']
     });
 
