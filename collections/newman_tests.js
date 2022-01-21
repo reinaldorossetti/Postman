7ce86861e5029bf5c9f7742e74142a47aa2a9119
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2022-01-21 18:37:26 -0300

    Create newman_tests.js

---
+++ collections/newman_tests.js
@@ -0,0 +1,24 @@
+'use strict';
+
+global.Promise = require('bluebird');
+const path = require('path');
+const newman =  Promise.promisifyAll(require('newman'));
+const fs = Promise.promisifyAll(require('fs'));
+const environment = 'postman_environment.json';
+const FOLDER = path.join(__dirname, 'Collections_Folder');
+
+
+let files = fs.readdirSync(FOLDER);
+files = files.map(file=> path.join(FOLDER, file))
+console.log(files);
+
+Promise.map(files, file => {
+    return newman.runAsync({
+    collection: file, // your collection
+    environment: path.join(__dirname, environment), //your env
+    reporters: ['cli']
+    });
+
+}, {
+   concurrency: 2
+});
